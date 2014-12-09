ef25a4987922480ed673d419c1d221957f032989
Schurik89 <viktorwich@googlemail.com>
2014-12-09 18:26:05 +0100

    Datenbank ist soweit eingerichtet

---
+++ app/db/dropTable.sql
@@ -0,0 +1,16 @@
+BEGIN TRANSACTION;
+
+DROP TABLE Buchung;
+DROP TABLE Fahrzeug;
+DROP TABLE Station;
+DROP TABLE Kunde;
+DROP TABLE Adresse;
+DROP TABLE Ort;
+
+COMMIT;
+
+--Ausgabe aller vorhandenen Tabellen.......
+SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;
+
+--BEI BUCHUNG DAZU
+--GesamtPreis	TEXT NOT NULL, --GesamtPreis JS-Funktion die den Gesamtpreis berechnet!
