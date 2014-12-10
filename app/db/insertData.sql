7b6cd4b9ea9bf0df8ae2b2f215d8bdd35e010329
hlk65 <luhansen@wi-pc61.in.fhkn.de>
2014-12-10 17:14:03 +0100

    JDBC hinzugefügt, Fahrzeugübersicht umgestellt

--- app/db/insertData.sql
+++ app/db/insertData.sql
@@ -1,5 +1,3 @@
-BEGIN TRANSACTION;
-
 insert into Ort (OrtsID,Ortsname) values(1,"Konstanz");
 insert into Ort (OrtsID,Ortsname) values(2,"Singen");
 insert into Ort (OrtsID,Ortsname) values(3,"Radolfzell");
@@ -12,14 +10,14 @@ insert into Station (StationsID,Adresse,Stationsname) values(111,11,"SK");
 insert into Station (StationsID,Adresse,Stationsname) values(222,22,"SS");
 insert into Station (StationsID,Adresse,Stationsname) values(333,33,"SR");
 
-insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(1,111,"Karosserieform: Limousine / Getriebe: Automatik / Antriebsart: Allrad / Hubraum: 2993cm3 / Kraftstoff: Diesel / Türen: 4 / Farbe: Schwarz","BMW","BMW X6","30","bmwx6.jpg");
+insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(1,111,"Karosserieform: Limousine / Getriebe: Automatik / Antriebsart: Allrad / Hubraum: 2993cm3 / Kraftstoff: Diesel / Türen: 4 / Farbe: Schwarz","BMW","X6","30","bmwx6.jpg");
 
-insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(2,111,"Karosserieform: Coupe / Getriebe: Automatik / Gänge: 7 / Türen: 3 / PS: 170 / Farbe: Weiss","Audi","Audi A3","20","audia3.jpg");
+insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(2,111,"Karosserieform: Coupe / Getriebe: Automatik / Gänge: 7 / Türen: 3 / PS: 170 / Farbe: Weiss","Audi","A3","20","audia3.jpg");
 
 insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(3,222,"Karosserieform: Coupe / Getriebe: Schaltgetriebe / Gänge: 6 / Türen: 3 / PS: 260 / Farbe: Weiss / Kraftstoff: Benzin","Volkswagen","Golf GTI","40","golfgti.jpg");
 
-insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(4,222,"Karosserieform: Coupe / Getriebe: Automatik / Gänge: 7 / Türen: 2 / PS: 210 / Farbe: Rot / Kraftstoff: Diesel","Honda","Honda CR-Z","35","honda.jpg");
+insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(4,222,"Karosserieform: Coupe / Getriebe: Automatik / Gänge: 7 / Türen: 2 / PS: 210 / Farbe: Rot / Kraftstoff: Diesel","Honda","CR-Z","35","honda.jpg");
 
-insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(5,333,"Karosserieform: Limousine / Getriebe: Schaltgetriebe / Gänge: 6 / Türen: 4 / PS: 150 / Farbe: Weiss / Kraftstoff: Benzin","Suzuki","Suzuki Swift","18","suzuki.jpg");
+insert into Fahrzeug (FahrzeugID,Station,Beschreibung,Hersteller,Modell,PreisProTag,Bild) values(5,333,"Karosserieform: Limousine / Getriebe: Schaltgetriebe / Gänge: 6 / Türen: 4 / PS: 150 / Farbe: Weiss / Kraftstoff: Benzin","Suzuki","Swift","18","suzuki.jpg");
 
 COMMIT;
--- app/model/Model.java
+++ app/model/Model.java
@@ -1,31 +1,43 @@
 package model;
+
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import play.db.DB;
 
 public class Model {
-	
-	private Model () {
+	private Model() {
 	}
 
-	public static Model sharedInstance = new Model();	
-	
-	private static ArrayList<Kunde> kunden= new ArrayList<Kunde>();
-	private static ArrayList<Adresse> adressen= new ArrayList<Adresse>();
-	private static ArrayList<Ausstattung> ausstattungen= new ArrayList<Ausstattung>();
-	private static ArrayList<Bild> bilder= new ArrayList<Bild>();
-	private static ArrayList<Fahrzeug> fahrzeuge= new ArrayList<Fahrzeug>();
-	private static ArrayList<Ort> orte= new ArrayList<Ort>();
-	private static ArrayList<Station> stationen= new ArrayList<Station>();
-	
-	public ArrayList<Kunde> getKunden(){
-		Kunde kunde1 = new Kunde("k1", "hans@peter.de", "Peter", "Hans", "0123142", "psw1");
-		Kunde kunde2 = new Kunde("k2", "hans@Gruetz.de", "Vogel", "Jörg", "0123143", "psw2");
-		Kunde kunde3 = new Kunde("k3", "hans@depp.de", "Götz", "Tom", "0123144", "psw3");
+	private static Connection connection = DB.getConnection();
+
+	public static Model sharedInstance = new Model();
+
+	private static ArrayList<Kunde> kunden = new ArrayList<Kunde>();
+	private static ArrayList<Adresse> adressen = new ArrayList<Adresse>();
+	// private static ArrayList<Ausstattung> ausstattungen = new
+	// ArrayList<Ausstattung>();
+	// private static ArrayList<Bild> bilder= new ArrayList<Bild>();
+	private static ArrayList<Fahrzeug> fahrzeuge = new ArrayList<Fahrzeug>();
+	private static ArrayList<Ort> orte = new ArrayList<Ort>();
+	private static ArrayList<Station> stationen = new ArrayList<Station>();
+
+	public ArrayList<Kunde> getKunden() {
+		Kunde kunde1 = new Kunde("k1", "hans@peter.de", "Peter", "Hans",
+				"0123142", "psw1");
+		Kunde kunde2 = new Kunde("k2", "hans@Gruetz.de", "Vogel", "Jörg",
+				"0123143", "psw2");
+		Kunde kunde3 = new Kunde("k3", "hans@depp.de", "Götz", "Tom",
+				"0123144", "psw3");
 		kunden.add(kunde1);
 		kunden.add(kunde2);
 		kunden.add(kunde3);
 		return kunden;
 	}
-	public ArrayList<Adresse> getAdressen(){
+
+	public ArrayList<Adresse> getAdressen() {
 		Adresse adresse1 = new Adresse("a1", "straße", "1", "13423");
 		Adresse adresse2 = new Adresse("a2", "straße", "2", "13423");
 		Adresse adresse3 = new Adresse("a3", "straße", "3", "13423");
@@ -34,24 +46,50 @@ public class Model {
 		adressen.add(adresse3);
 		return adressen;
 	}
-	public ArrayList<Ausstattung> getAusstattungen(){
-		Ausstattung ausstattung1 = new Ausstattung("as1", "Klima", "Super Kalt");
-		ausstattungen.add(ausstattung1);
-		return ausstattungen;
-	}
-	public ArrayList<Fahrzeug> getFahrzeuge(){
-		Fahrzeug fahrzeug1 = new Fahrzeug("f1", "SUV der Marke BMW, 300 PS", "BMW", "X6", "100", "/assets/images/bmwx6.jpg");
-		Fahrzeug fahrzeug2 = new Fahrzeug("f2", "Klasse Auto", "Audi", "A3", "90", "/assets/images/audia3.jpg");
-		fahrzeuge.add(fahrzeug1);
-		fahrzeuge.add(fahrzeug2);
+
+	// public ArrayList<Ausstattung> getAusstattungen() {
+	// Ausstattung ausstattung1 = new Ausstattung("as1", "Klima", "Super Kalt");
+	// ausstattungen.add(ausstattung1);
+	// return ausstattungen;
+	// }
+
+	public ArrayList<Fahrzeug> getFahrzeuge() {
+		try {
+			PreparedStatement pstmt = connection
+					.prepareStatement("SELECT * FROM Fahrzeug");
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Fahrzeug fahrzeug = new Fahrzeug(
+						rs.getString("FahrzeugId"), 
+						rs.getString("Beschreibung"), 
+						rs.getString("Hersteller"), 
+						rs.getString("Modell"), 
+						rs.getString("preisProTag"), 
+						"/assets/images/"+rs.getString("Bild")
+						);
+				fahrzeuge.add(fahrzeug);
+			}
+		} catch (SQLException e) {
+			System.out.println("Fehler beim laden der Fahrzeuge");
+			e.printStackTrace();
+		}
+
+		// Fahrzeug fahrzeug1 = new Fahrzeug("f1", "SUV der Marke BMW, 300 PS",
+		// "BMW", "X6", "100", "/assets/images/bmwx6.jpg");
+		// Fahrzeug fahrzeug2 = new Fahrzeug("f2", "Klasse Auto", "Audi", "A3",
+		// "90", "/assets/images/audia3.jpg");
+		// fahrzeuge.add(fahrzeug1);
+		// fahrzeuge.add(fahrzeug2);
 		return fahrzeuge;
 	}
-	public ArrayList<Ort> getOrte(){
+
+	public ArrayList<Ort> getOrte() {
 		Ort ort1 = new Ort("o1", "Konstanz");
 		orte.add(ort1);
 		return orte;
 	}
-	public ArrayList<Station> getStationen(){
+
+	public ArrayList<Station> getStationen() {
 		Station station1 = new Station("s1", "Station1");
 		stationen.add(station1);
 		return stationen;
