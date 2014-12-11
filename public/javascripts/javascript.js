3c13111cede915487d72c2e861735900b0450956
Schurik89 <viktorwich@googlemail.com>
2014-12-11 12:34:51 +0100

    KontaktSeite komplett eingerichtet mit JS usw..

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -5,3 +5,35 @@
 	function picktime (){
 	$('.timeauswahl').pickatime();
 	}
+	
+	function checkKontakt( form ){
+    
+	var warnung = "";
+    if ( form.email.value == "" ) {
+      warnung = "Bitte geben Sie ihre E-Mail-Adresse ein!\n";
+	  alert(warnung);
+	  document.formular.email.focus();
+	  return false;
+    }
+	if ( form.name.value == "" ) {
+      warnung = "Bitte geben Sie ihren Namen ein!\n";
+	  alert(warnung);
+	  document.formular.name.focus();
+	  return false;
+    }
+    if ( form.nachricht.value == "" ) {
+      warnung = "Sie haben Ihr Anliegen noch nicht beschrieben!\n";
+	  alert(warnung);
+	  document.formular.nachricht.focus();
+	  return false;
+    }
+        
+  }
+	
+	
+	
+	
+	
+	
+	
+	
