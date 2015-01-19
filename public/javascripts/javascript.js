370a999957714dc5aaaa99afda69ac9ca30542b9
hlk65 <luhansen@WI-PC12.in.fhkn.de>
2015-01-19 16:59:02 +0100

    javascript.js aufgeräumt + pickdate und picktime eingedeutscht

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -2,42 +2,16 @@ function pickdate() {
 	$('.dateauswahl').pickadate({
 		format : 'dd.mm.yyyy',
 		today : 'Heute',
-		clear : 'Loeschen',
+		clear : 'Löschen',
 		close : 'Schliessen',
-		min : 'true'
+		min : 'true',
+		monthsFull: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+		weekdaysShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
 	});
 }
 function picktime() {
 	$('.timeauswahl').pickatime({
 		format : 'HH:i',
+		clear : 'Löschen',
 	});
-}
-
-function checkKontakt(form) {
-
-	var warnung = "";
-	if (form.email.value == "") {
-		warnung = "Bitte geben Sie ihre E-Mail-Adresse ein!\n";
-		alert(warnung);
-		document.formular.email.focus();
-		return false;
-	}
-	if (form.name.value == "") {
-		warnung = "Bitte geben Sie ihren Namen ein!\n";
-		alert(warnung);
-		document.formular.name.focus();
-		return false;
-	}
-	if (form.nachricht.value == "") {
-		warnung = "Sie haben Ihr Anliegen noch nicht beschrieben!\n";
-		alert(warnung);
-		document.formular.nachricht.focus();
-		return false;
-	}
-
-}
-
-// Element entfernen
-function elementEntfernen(e) {
-	element.parentNode.removeChild(e);
 }
