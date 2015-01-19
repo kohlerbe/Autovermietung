06a445e7255aa60c1e4a533769714c74837f8028
hlk65 <luhansen@WI-PC12.in.fhkn.de>
2015-01-19 14:44:00 +0100

    ajax in eigene .js ausgelagert

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -37,76 +37,7 @@ function checkKontakt(form) {
 
 }
 
-// AJAX NR 1 /fahrzeugübersicht mit Button
-function dynamischesLaden() {
-	$('#ajax').load(
-			'/fahrzeuge',
-			function(responseTxt, statusTxt, xhr) {
-				if (statusTxt == "sucess") {
-					console.log("Erfolgreich!");
-				} else if (statuTxt == "error") {
-					alert("Ajax ERROR with status " + xhr.status + ": "
-							+ xhr.statusText);
-				}
-			});
-	var element = document.getElementById("button");
-	elementEntfernen(element);
-}
-
 // Element entfernen
 function elementEntfernen(e) {
 	element.parentNode.removeChild(e);
 }
-
-// AJAX NR 2 /fahrzeugübersicht Aktuell
-$(document).ready(function() {
-	count = 0;
-	start = 4;  //Anzahl Fahrzeuge die immer ausgegeben werden
-	lock = false;
-	for (i = 0; i < start; i++) {
-		if (lock == false) {
-			loadcontent();
-		}
-	}
-});
-
-// AJAX mit Scroll Function
-function loadcontent() {
-	lock = true; //Um paralleles Ausführen zu verhindern
-	console.log("In Loadcontent mit count: " + count);
-	$("div.loader").show();
-
-	$.ajax({
-		type : 'get',
-		url : '/fahrzeuge',
-		async : false,
-		data : {
-			count : count
-		},
-
-		success : function(html) {
-			if (html) {
-				$('#container').append(html);
-				count += 1;
-				lock = false;
-			}
-			;
-		},
-
-	})
-
-	$("div.loader").fadeOut('normal');
-	console.log("Verlasse Loadcontent mit count: " + count);
-	lock = false;
-}
-
-$(window).scroll(
-		function() {
-			if (i >= start
-					&& $(window).scrollTop() == $(document).height()
-							- $(window).height()) {
-				if (lock == false) {
-					loadcontent();
-				}
-			}
-		});
